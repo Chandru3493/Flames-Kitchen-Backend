e71fba82a615db44a7bcad1b573830e4c3ae0134
Anurag14092001 <104491006+Anurag14092001@users.noreply.github.com>
2024-02-28 22:57:43 +0530

    Update 20240227105758-orderitem.js

--- seeders/20240227105758-orderitem.js
+++ seeders/20240227105758-orderitem.js
@@ -5,71 +5,83 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     
      await queryInterface.bulkInsert('OrderItem', [{
-      order_id: 1
+      order_id: 1,
+    
       ,
+       cook_id: null,
+       status:'todo',
       menu_item_id: 1,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 2,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 3,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 4,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      ,cook_id: null,
+       status:'todo',
       menu_item_id: 5,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 6,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 7,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 8,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      ,cook_id: null,
+       status:'todo',
       menu_item_id: 9,
       quantity: 1,
       
       price: 100
      },{
       order_id: 1
-      ,
+      , cook_id: null,
+       status:'todo',
       menu_item_id: 10,
       quantity: 1,
       
