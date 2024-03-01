ad14aa7e4cb19d812d9755fc65be13ef3bd2ebff
sanjay <sanjayg.mct2020@citchennai.net>
2024-03-01 20:28:08 +0530

    msg

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 4000;
 //const { Employee} = require('./models')
 const {sequelize,pool} = require('./db.js');
+const {Op}= require('sequelize');
 const AnuragEmp= require('./models/employee_data.js');
 const Login = require('./models/login.js');
 const FinancialDay =require('./models/financial_day_data.js');
@@ -13,6 +14,7 @@ const Role =  require('./models/role.js')
 const Salary = require('./models/salary.js');
 
 const bcrypt = require('bcrypt');
+const Employee = require('./models/employee_data.js');
 (async () => {
   await sequelize.sync({logging:false,force:false}); // Use `{ force: true }` with caution, as it drops existing tables
   console.log('Tables synchronized successfully');
@@ -77,8 +79,18 @@ app.post("/datas", async (req, res) => {
 
 app.get("/datas", async (req, res) => {
     try {
-      const data = await AnuragEmp.findAll()
-      res.json(data.rows);
+      const data = await AnuragEmp.findAll({include:[Role,Salary],
+  
+      })
+      const d = []
+      data.forEach((item)=>{
+        const v = item.dataValues.role.dataValues;
+        const c = item.dataValues.salary.dataValues;
+            d.push({...item.dataValues,salary: c.emp_salary,role : v.rolename});
+      })
+    
+      res.json(d);
+  
     } catch (err) {
       console.error(err.message);
     }
@@ -88,7 +100,10 @@ app.get("/datas/:username", async (req, res) => {
     const username=req.params.username
     try {
       const data = await AnuragEmp.findAll({include:[Role,Salary],
-        where:{name:username,},
+        where:{[Op.or]: [
+          {name: username },
+          //{id: Number(username)},
+        ],},
       })
       const d = []
       data.forEach((item)=>{
@@ -126,21 +141,43 @@ app.get("/datas/:username", async (req, res) => {
 
 app.put("/datas/:id", async (req,res) => {
   const { id } = req.params;
-  const { description } = req.body;
+  const { name,email,address,role,salary } = req.body;
+  
+  //
+  const exists = await AnuragEmp.findAll({where: {email_id:email,id:{[Op.ne]:id}}});
+  if (exists.length===0){
+    if(role==="admin"){
+      var ri=1;
+    }else if(role==="waiter"){
+      var ri=2;
+    }else{
+      var ri=3;
+    }
+  //
     try {
       const data = await Salary.update(
-        {emp_salary: Number(description)},
+        {emp_salary: Number(salary)},
         {where: {empid: id},}
       );
+      const data1 = await AnuragEmp.update(
+        {email_id:email,name:name,address:address,roleId:ri},
+        {where: {id: id},}
+      );
+
       const x =[];
       data.forEach((item)=>{
         x.push(item.dataValues);
       })
-    res.json(data);
+    res.json(exists);
     }catch (err) {
         console.error(err.message);
-      }});
-
+      }
+    //
+    }else{
+      res.send(exists)
+    }
+    //
+      });
 
 app.get('/day', async (req,res)=>{
   const resu = await  FinancialDay.findAll({where:{date: req.query.datestamp}});
